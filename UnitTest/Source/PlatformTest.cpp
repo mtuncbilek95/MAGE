a874dbd83f8628792f794be99cc8f109eb0b7441
Metehan Tuncbilek <mtuncbilek95@gmail.com>
2024-09-06 21:43:38 +0300

    dead removal skin

--- UnitTest/Source/PlatformTest.cpp
+++ UnitTest/Source/PlatformTest.cpp
@@ -27,22 +27,10 @@ TEST_CASE("Platform Directory")
 	REQUIRE(PlatformDirectory::Exists("UnitTest"));
 	REQUIRE(PlatformDirectory::Delete("UnitTest"));
 	REQUIRE(!PlatformDirectory::Exists("UnitTest"));
-
-	Vector<String> files = PlatformDirectory::GetFiles("CMakeFiles/UnitTest.dir");
-	REQUIRE(files.size() > 0);
-
-	Vector<String> filteredFiles = PlatformDirectory::GetFilesByExtension("CMakeFiles/UnitTest.dir", "obj");
-	REQUIRE(filteredFiles.size() > 0);
-
-	Vector<String> directories = PlatformDirectory::GetDirectories("..");
-	REQUIRE(directories.size() > 0);
 }
 
 TEST_CASE("Platform File")
 {
-	REQUIRE(PlatformFile::Exists("CMakeFiles/UnitTest.dir/CoreTest.cpp.obj"));
-	REQUIRE(!PlatformFile::Exists("CMakeFiles/UnitTest.dir/main.cpp.obj"));
-	REQUIRE(PlatformFile::GetSize("CMakeFiles/UnitTest.dir/CoreTest.cpp.obj") > 0);
 }
 
 TEST_CASE("Platform Hardware")
